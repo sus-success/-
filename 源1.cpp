8f49957338c9ffbc618b0519269d24fbfadc2389
sus-success <2029283347@qq.com>
2022-04-24 23:25:07 +0800

    第二天

---
+++ 源1.cpp
@@ -0,0 +1,74 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//#define mac 100000
+//int main()
+//{
+//	int n =mac;
+//	printf("%d\n", n = mac);
+//
+//
+////		return 0;
+////}
+//enum sex
+//{
+//	male,
+//	female,
+//	secret,
+//};
+//int main()
+//{
+//	enum sex s = male;
+//	printf("%d\n",male);
+//	printf("%d\n",female);
+//	printf("%d\n",secret);
+//       return 0;
+//}
+//#include <string.h>
+//int main()
+//{
+//	////char arr[] = "hello";
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c'};
+//	//printf("%s\n", arr1);
+//	//printf("%s\n", arr2);
+//	//int len = strlen("abc");
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	return 0;
+//}
+//printf("%d\n", strlen("c:\test\x32\8\test.c"));
+//#include <string.h>
+//int main()
+//{
+//	int input = 100;
+//		printf("面试成功会成功吗？");
+//	scanf("%d", &input);
+//	if (input == 1)
+//		printf("yes\n");
+//	else
+//		printf("no \n");
+//	return 0;
+//
+//}
+int main()
+{
+	int input = 100;
+	printf("决定学编程了吗（yes/no)？");
+	scanf("%d", &input);
+	if (input == 1)
+		printf("yes\n");
+	else
+		printf("no \n");
+
+	int line = 0;
+	while (line < 10000)
+	{
+		printf("写代码天数:%d\n",line);
+		line++;
+	}
+	if (line = 10000)
+	{
+		printf("你得到了一个好offer（￣︶￣）↗\n");
+	   }
+} 
+
