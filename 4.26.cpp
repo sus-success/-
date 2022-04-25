7d5bb94bc1598efac73328b976e84765b335c7dc
sus-success <2029283347@qq.com>
2022-04-26 00:04:00 +0800

    第三天

---
+++ 4.26.cpp
@@ -0,0 +1,185 @@
+﻿#define _crt_secure_no_warnings 1
+#include <stdio.h>
+//#define mac 100000
+//int main()
+//{
+//	int n =mac;
+//	printf("%d\n", n = mac);
+//
+//
+////		return 0;
+////}
+//enum sex
+//{
+//	male,
+//	female,
+//	secret,
+//};
+//int main()
+//{
+//	enum sex s = male;
+//	printf("%d\n",male);
+//	printf("%d\n",female);
+//	printf("%d\n",secret);
+//       return 0;
+//}
+//#include <string.h>
+//int main()
+//{
+//	////char arr[] = "hello";
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c'};
+//	//printf("%s\n", arr1);
+//	//printf("%s\n", arr2);
+//	//int len = strlen("abc");
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	return 0;
+//}
+//printf("%d\n", strlen("c:\test\x32\8\test.c"));
+//#include <string.h>
+//int main()
+//{
+//	int input = 100;
+//		printf("面试成功会成功吗？");
+//	scanf("%d", &input);
+//	if (input == 1)
+//		printf("yes\n");
+//	else
+//		printf("no \n");
+//	return 0;
+//
+////}
+//int main()
+//{
+//	int input = 100;
+//	printf("决定学编程了吗（yes/no)？");
+//	scanf("%d", &input);
+//	if (input == 1)
+//		printf("yes\no");
+//	else
+//		printf("no \no");
+//
+//	int line = 0;
+//	while (line < 10000)
+//	{
+//		printf("写代码天数:%d\n",line);
+//		line++;
+//	}
+//	if (line = 10000)
+//	{
+//		printf("你得到了一个好offer（￣︶￣）↗\n");
+//	   }
+//} 
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int num1 = 1;
+//	int num2 = 3;
+//	scanf("%d%d", &num1, &num2);
+//	int sum=Add(num1, num2);
+//	printf("%d\n", sum);
+//	return 0;
+////}
+//int main()
+//{
+	//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	//	char ch[5] = { 'a','b','c' };
+	//	return 0;
+	//}
+	//int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	//int i = 9;
+	//while (i < 10)
+	//{
+	//	printf("%d ", arr[i]);
+	//	i++;
+	//}
+///*	int a = 9 % 2;
+//	printf("%d\n", a)*/;
+//	int a = 7;
+//	int b = a >>1;
+//	printf("%d\n", b);
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[8] = { 0,1 };
+//	printf("%d\n",sizeof( arr));
+//	printf("%d\n",sizeof( arr[0]));
+//	int a = sizeof(arr) / sizeof(arr[0]);
+//	printf("%d\n", sizeof(a));
+//	return 0;
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = (a++) + (a++) + (a++);
+//	int c = a;
+//
+//
+//	printf("%d\n", c);
+//	return 0;
+////}
+//int main()
+//{
+//	int a = (int)3.14;
+//	printf("%d\n", a);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 3;
+//	int b = 2;
+//	int max = 0;
+//	max = a < b ? a : b;
+//	printf("%d\n", max); 
+//		
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 1;
+//	int c = 2;
+//	int d = (a = b + 2, b = a + 3, c = b+ 4);
+//	printf("%d\n", d);
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	printf("%d\n", arr[5]);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//
+//
+//	int a = 10;
+//	printf("%d\n", a);
+//	return 0;
+//}
+//extern int Ad(int x, int y);
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int sum = Ad(a, b);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+#define add(x,y) (x+y)
+int main()
+{
+
+	printf("%d\n", 4*add(2,3));
+	return 0;
+}
